268b7500bd1c24fa7b67052bc15a1e8d4b51dae3
ThalesNagato <nagatothales@gmail.com>
2024-09-14 22:59:50 -0400

    Add end screen functionality

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+<<<<<<< Updated upstream
 using System.Threading;
 using Unity.VisualScripting;
+=======
+>>>>>>> Stashed changes
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public float restartTime = 1;
 
     private float timer = 0;
@@ -37,3 +41,26 @@ public class LoadScene : MonoBehaviour
 
 
 }
+=======
+    public float interpolate = 1;
+    public float fadeSpeed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        interpolate -= fadeSpeed * Time.deltaTime;
+        GetComponent<CanvasGroup>().alpha = interpolate;
+
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene("FINAL SCENE");
+        }
+    }
+}
+>>>>>>> Stashed changes
--- Assets/Scripts/Ui/FadeToBlack.cs
+++ Assets/Scripts/Ui/FadeToBlack.cs
@@ -40,14 +40,13 @@ public class FadeToBlack : MonoBehaviour
 
             if (timer >= blackTime)
             {
-                interpolate = 0;
-                isFading = false;
+
                 Conductor.songPositionInBeats = 0;
                 Conductor.dspSongTime = 0;
-                SceneManager.LoadScene("FINAL SCENE");
+                SceneManager.LoadScene("Failure");
 
 
-                GetComponent<Canvas>().sortingOrder = -1;
+              
             }
 
 
